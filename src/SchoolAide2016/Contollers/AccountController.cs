0c1bb06cd55c77723b8f76ceb89c86e0d0e6020d
ecco88 <bcaputo1@comcast.net>
2016-04-08 01:05:43 -0400

    added google - need to get a google key

--- src/SchoolAide2016/Contollers/AccountController.cs
+++ src/SchoolAide2016/Contollers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using System.Security.Claims;
+using Microsoft.AspNet.Http.Authentication;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -40,6 +41,24 @@ namespace SchoolAide2016.Contollers
                 return new LocalRedirectResult(returnUrl);
             }
             return View();
+        }
+
+        public async Task<IActionResult> Google()
+        {
+            var props = new AuthenticationProperties
+            {
+                RedirectUri = "/home/secure/"
+            };
+            //return ChallengeResult("Google")
+            await HttpContext.Authentication.ChallengeAsync("Google", props);
+
+            return new ChallengeResult("Google", props);
+        }
+        [HttpGet]
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookites");
+            return Redirect("/");
         }
     }
 }
